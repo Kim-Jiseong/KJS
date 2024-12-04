9f2f52b6cc15e9eb844b3e9c5055e2afe6efac08
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2024-12-04 15:48:45 +0900

    v0.2.0 - internalization without i18n

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { ThemeProvider, ThemeProviderProps } from "next-themes";
-import { ReactNode } from "react";
-import { I18nextProvider } from "react-i18next";
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
